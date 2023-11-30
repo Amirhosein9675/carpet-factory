de32313045400efbb0e560423480246ab0ee6311
amirhossein <amir.hossein.1874@gmail.com>
2023-12-01 02:14:40 +0330

    fix urls

--- Carpet/urls.py
+++ Carpet/urls.py
@@ -4,12 +4,13 @@ from .api import *
 urlpatterns = [
 
     path('register/',RegisterUser.as_view(),name=''),
-    path('carpet/all-carpet-list/',GetCarpet.as_view(),name='getcarpet'),
+    path('carpet/all-carpets-list/',GetCarpet.as_view(),name='getcarpet'),
     path('carpet/detail/<pk>/',CarpetDetails.as_view(),name='carpetdetail'),
-    path('get/detail/serviceprovider/',GetDetailServiceprovider.as_view(),name='getserviceprovider'),
-    path('get/services/',GetServices.as_view(),name='getservices'),
+    path('serviceprovider/all-serviceproviders-list/',GetDetailServiceprovider.as_view(),name='getserviceprovider'),
+    path('services/all-services-list/',GetServices.as_view(),name='getservices'),
     
-    path('carpet/rigister-from-excel/',CarpetFromExcel.as_view()),
+    path('carpet/register-from-excel/',CarpetFromExcel.as_view()),
+    
     path('updateservice/',UpdateServiceProviders.as_view(),name='updateservice'),
     path('getstatus/',GetStatus.as_view(),name='getsatus'),
     path('updatecarpet/',UpdateCarpet.as_view(),name='updatecarpet'),
